0cd641e3090576890aa8268847c208de1e04172c
Jhonathan <jhonathasouza88@gmail.com>
2025-09-23 20:24:13 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const Products = () => {
+  return <div>
+    <div>hello</div>
+    <Button>mequi</Button>
+    <Input placeholder="digite..."/>
+  </div>;
+};
+
+export default Products;
