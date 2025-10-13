0c3aa0fadb3e12eaedfb2d2d9d94c6e0efbdeb5c
Jhonathan <jhonathasouza88@gmail.com>
2025-10-13 19:20:21 -0300

    feat: add product header component

---
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface ProductHeaderProps{
+    product: Pick<Product, "name" | "imageUrl">
+}
+
+const ProductHeader = ({product}: ProductHeaderProps) => {
+
+    const router = useRouter()
+    const handleBackClick = ()=> router.back()
+    return ( 
+        <div className="relative w-full h-[300px]">
+            <Button
+                className="absolute left-4 top-4 z-50 rounded-full"
+                variant="secondary"
+                size="icon"
+                onClick={handleBackClick}
+            >
+                <ChevronLeftIcon />
+            </Button>
+            <Button
+                className="absolute right-4 top-4 z-50 rounded-full"
+                variant="secondary"
+                size="icon"
+            >
+                <ScrollTextIcon />
+            </Button>
+            <Image
+                src={product.imageUrl}
+                alt={product.name}
+                fill
+                className="object-contain"
+            />
+        </div>
+     );
+}
+ 
+export default ProductHeader;
